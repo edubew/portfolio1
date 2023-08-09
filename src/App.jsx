e38a110198faba23c7fb826fbce9148f567221bb
edubew <edubew98@gmail.com>
2023-08-09 13:25:18 +0300

    Style the about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/header/Header';
+import About from './components/about/About';
 
 function App() {
 
   return (
     <div>
       <Header />
+      <About />
     </div>
   )
 }
